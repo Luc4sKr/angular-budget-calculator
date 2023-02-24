225acf7d6ac9b23fbcdde7f3a2087a0295722e46
Lucas <lucaskreuch77@gmail.com>
2023-02-23 22:37:57 -0300

    update budget-item-card

--- src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-budget-item-card',
   templateUrl: './budget-item-card.component.html',
   styleUrls: ['./budget-item-card.component.scss']
 })
-export class BudgetItemCardComponent {
+export class BudgetItemCardComponent implements OnInit {
+  @Input() isIncome: boolean = true;
 
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 }
