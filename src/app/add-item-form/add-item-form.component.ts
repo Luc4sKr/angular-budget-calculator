5243e741b86d71591b91f385c0f76c72ceb590a6
Lucas <lucaskreuch77@gmail.com>
2023-02-25 20:40:58 -0300

    update item-form

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+//import { BudgetItem } from 'src/shared/models/budget-item.model';
+
 
 @Component({
   selector: 'app-add-item-form',
   templateUrl: './add-item-form.component.html',
   styleUrls: ['./add-item-form.component.scss']
 })
-export class AddItemFormComponent {
+export class AddItemFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form)
+  }
 
 }
